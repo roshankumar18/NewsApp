0a91e031f2f2d2706ded01725e8b38d236ef8cbb
rosteve10 <houseno14a@gmail.com>
2021-07-27 11:07:14 +0530

    Add files via upload

---
+++ newsfeed/utils/Constants.java
@@ -0,0 +1,100 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Soojeong Shin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.example.android.newsfeed.utils;
+
+
+
+public class Constants {
+
+
+    private Constants() {
+    }
+
+
+    static final String JSON_KEY_RESPONSE = "response";
+    static final String JSON_KEY_RESULTS = "results";
+    static final String JSON_KEY_WEB_TITLE = "webTitle";
+    static final String JSON_KEY_SECTION_NAME = "sectionName";
+    static final String JSON_KEY_WEB_PUBLICATION_DATE = "webPublicationDate";
+    static final String JSON_KEY_WEB_URL = "webUrl";
+    static final String JSON_KEY_TAGS = "tags";
+    static final String JSON_KEY_FIELDS = "fields";
+    static final String JSON_KEY_THUMBNAIL = "thumbnail";
+    static final String JSON_KEY_TRAIL_TEXT = "trailText";
+
+
+    static final int READ_TIMEOUT = 10000; /* milliseconds */
+
+
+    static final int CONNECT_TIMEOUT = 15000; /* milliseconds */
+
+
+    static final int SUCCESS_RESPONSE_CODE = 200;
+
+
+    static final String REQUEST_METHOD_GET = "GET";
+
+
+    public static final String NEWS_REQUEST_URL = "https://content.guardianapis.com/search";
+
+
+    public static final String QUERY_PARAM = "q";
+    public static final String ORDER_BY_PARAM = "order-by";
+    public static final String PAGE_SIZE_PARAM = "page-size";
+    public static final String ORDER_DATE_PARAM = "order-date";
+    public static final String FROM_DATE_PARAM = "from-date";
+    public static final String SHOW_FIELDS_PARAM = "show-fields";
+    public static final String FORMAT_PARAM = "format";
+    public static final String SHOW_TAGS_PARAM = "show-tags";
+    public static final String API_KEY_PARAM = "api-key";
+    public static final String SECTION_PARAM = "section";
+
+
+    public static final String SHOW_FIELDS = "thumbnail,trailText";
+
+
+    public static final String FORMAT = "json";
+
+
+    public static final String SHOW_TAGS = "contributor";
+
+
+    public static final String API_KEY = "test";
+
+
+    public static final int DEFAULT_NUMBER = 0;
+
+
+    public static final int HOME = 0;
+    public static final int WORLD = 1;
+    public static final int SCIENCE = 2;
+    public static final int SPORT = 3;
+    public static final int ENVIRONMENT = 4;
+    public static final int SOCIETY = 5;
+    public static final int FASHION = 6;
+    public static final int BUSINESS = 7;
+    public static final int CULTURE = 8;
+
+}
